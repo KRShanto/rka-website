7f1bcfc43b99026b1f7220385b264c2fdadba435
KR Shanto <krshanto2005@gmail.com>
2025-09-24 20:54:16 +0600

    feat(config): update Next.js configuration to support remote images
    - Added remotePatterns to the Next.js configuration for handling images from a specified hostname.
    - Enhanced image loading capabilities by allowing images from external sources.

--- next.config.mjs
+++ next.config.mjs
@@ -1,2 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "hebbkx1anhila5yf.public.blob.vercel-storage.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
